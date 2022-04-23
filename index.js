07e631a7b6c3751c4e0a82efa0b4a9faacbdc523
Abhinav R <abhinav7r@gmail.com>
2022-04-24 02:41:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+    let { url, method, headers } = request;
+    if(url === '/') {
+        let html = readFile('./pages/home.html');
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.write(html);
+        response.end();
+    }
+    else if(url === '/about') {
+        let html = readFile('./pages/about.html');
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.write(html);
+        response.end();
+    }
+    else {
+        response.writeHead(404, {'Content-Type':'text/plain'});
+        response.end('Page does not exist');
+    }
+    
+});
+
+function readFile(path) {
+    return fs.readFileSync(path, (error, data) => {
+        if(error) {
+            throw error;
+        }
+        return data;
+    });
+}
+
+server.listen(8080);
